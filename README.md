dd6dc4feb58f98bd9e499c0a0df642b769b834aa
Mariano Sciacco <marianosciacco@gmail.com>
2018-12-13 21:05:29 +0100

    Fix e implementazione del modello, Aggiunti badge al README #2 #3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # A2-TOS-CI
 Assignment #2 - Tecnologie Open Source, UniPD Informatica (Mariano Sciacco)
+
+### Status della Build
+
+[![Build Status](https://travis-ci.org/Maxelweb/A2-TOS-CI.svg?branch=master)](https://travis-ci.org/Maxelweb/A2-TOS-CI)
+
+### Code Coverage
+
+[![Coverage 
+Status](https://coveralls.io/repos/github/Maxelweb/A2-TOS-CI/badge.svg?branch=master)](https://coveralls.io/github/Maxelweb/A2-TOS-CI?branch=master)
--- src/main/java/it/unipd/tos/model/MenuItem.java
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -1,5 +1,37 @@
+////////////////////////////////////////////////////////////////////
+// [Mariano] [Sciacco] [1142498]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos.model;
 
 public class MenuItem {
+
+    public enum items { PIZZA, PRIMO }
+
+    private items itemType;
+    private String name;
+    private double price;
+
+    public MenuItem(String n, items t, double p) 
+    {
+        this.itemType = t;
+        this.name = n;
+        this.price = p;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    
+    public items getType()
+    {
+        return itemType;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
 
 }
