0943f7cf3de0603b31664e266674985a71011d59
Mariano Sciacco <marianosciacco@gmail.com>
2018-12-13 20:50:21 +0100

    Configurazione dei plugin e predisposizione Travis CI #1 #2 #3

--- src/main/java/it/unipd/tos/App.java
+++ src/main/java/it/unipd/tos/App.java
@@ -1,13 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// [Mariano] [Sciacco] [1142498]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Implementazione di un programma per gli scontrini
     }
 }
